b72a767b7c2c2d6c0175405d1a61b3febc76a3fa
unknown <sanjeetkumarit@outlook.com>
2024-12-17 23:54:32 +0530

    modified generator script to create component in React

--- generateComponent.js
+++ generateComponent.js
@@ -9,17 +9,26 @@ if (!componentName) {
     process.exit(1);
 }
 
-// Define the path to your new component
-const componentDir = path.join(__dirname, 'src', 'components', componentName);
 
-if (fs.existsSync(componentDir)) {
-    console.error("Component already exists!");
+// Define the path to the new component file directly in the components directory
+const componentDir = path.join(__dirname, 'src', 'components');
+const componentPath = path.join(componentDir, `${componentName}.tsx`);
+
+// Check if the component directory exists, if not, create it
+if (!fs.existsSync(componentDir)) {
+    fs.mkdirSync(componentDir, { recursive: true });
+  }
+  
+  // Check if a file or directory already exists with the same name
+  if (fs.existsSync(componentPath)) {
+    console.log(`Error: A file named '${componentName}.tsx' already exists.`);
     process.exit(1);
-}
+  }
+  
 
 // Create the directory recursively (if it doesn't already exist)
 //{ recursive: true } to ensure that all the necessary parent directories (src or components) are created automatically.
-fs.mkdirSync(componentDir, { recursive: true });
+//fs.mkdirSync(componentPath, { recursive: true });
 
 // Create the component file
 const componentContent = `
@@ -37,10 +46,9 @@ export default ${componentName};
 `;
 
 // Write the component file
-fs.writeFileSync(
-    path.join(componentDir, `${componentName}.tsx`),
-    componentContent.trim()
-);
+fs.writeFileSync(componentPath, componentContent);
 
-console.log(`${componentName} component created at ${componentDir}`);
+console.log(`${componentName} component created at ${componentPath}`);
 
+//Run in local: node generateComponent.js Editor
+//to remove directory from command line : rmdir /s /q D:\Sanjeet\Repo\react-tinymce-form\src\components\Editor.tsx
